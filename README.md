be4fe3922488ecd670c42fbfa761a5634301a74c
implodingduck <zsek320@gmail.com>
2021-10-22 11:10:56 -0500

    adding tiles

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Fan creation for the game [Swords n Magic and Stuff](https://store.steampowered.
 
 The purpose of this project is to create a custom map with labels to easily find different resources and NPCs.
 
+https://implodingduck.github.io/swords-n-magic-and-maps/
+
+## Local Setup
+```
+conda create -n gdal
+conda activate gdal
+conda install -c conda-forge gdal
+gdal2tiles.py swords-n-magic-and-maps.tif -p raster 
+```
+
 ## Resources
 * https://store.steampowered.com/app/810040/Swords_n_Magic_and_Stuff/
 * https://dev.to/maj07/tutorial-react-leaflet-d65
--- app/src/Map.js
+++ app/src/Map.js
@@ -2,18 +2,18 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import useWindowDimensions from "./UseWindowDimensions";
 
 const Map = () => {
-    const defaultPosition = [48.864716, 2.349]; 
+    const defaultPosition = [0 ,0]; 
     const { height, width } = useWindowDimensions();
     return (
         <div className="map__container" style={ { border: "1px solid #ff00ff" } }>
             <MapContainer
                 center={defaultPosition}
-                zoom={3}
+                zoom={1}
                 style={ { height: height + "px" } }
             >
                 <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='Map From Kindred Games: Swords n Magic and Stuff'
+                url="https://implodingduck.github.io/swords-n-magic-and-maps/tiles/{z}/{x}/{y}.png"
                 />
             </MapContainer>
         </div>
