be34ab155aff685bd8c8e09792ec17e4671586df
implodingduck <zsek320@gmail.com>
2021-10-22 11:45:14 -0500

    adjustments for non geo tiles

--- app/src/Map.js
+++ app/src/Map.js
@@ -2,21 +2,22 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import useWindowDimensions from "./UseWindowDimensions";
 
 const Map = () => {
-    const defaultPosition = [0 ,0]; 
+    const defaultPosition = [-60 ,-30]; 
     const { height, width } = useWindowDimensions();
     return (
         <div className="map__container" style={ { border: "1px solid #ff00ff" } }>
             <MapContainer
                 center={defaultPosition}
-                zoom={1}
+                zoom={3}
                 style={ { height: height + "px" } }
-                minZoom={0}
-                maxZoom={3}
-                maxNativeZoom={3}
             >
                 <TileLayer
                 attribution='Map From Kindred Games: Swords n Magic and Stuff'
-                url="https://implodingduck.github.io/swords-n-magic-and-maps/tiles/{z}/{y}/{x}.png"
+                url="https://implodingduck.github.io/swords-n-magic-and-maps/tiles/{z}/{x}/{y}.png"
+                minZoom={2}
+                maxNativeZoom={3}
+                noWrap={true}
+                tms={true}
                 />
             </MapContainer>
         </div>
